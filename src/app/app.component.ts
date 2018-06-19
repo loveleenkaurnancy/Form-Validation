48e469cd632124051732f54ef57fa67c0ae88759
loveleen <loveleen.nancy@gmail.com>
2018-06-19 22:40:50 +0530

    Submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  pin : string = '12345';
+
+  @ViewChild('signUpForm') signForm: NgForm;
+  onSubmit() {
+    console.log(this.signForm.value);
+  }
+
 }
